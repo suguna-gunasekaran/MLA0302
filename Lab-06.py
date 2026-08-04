f7540cd647dd7342bd656a5185a424526cded5a0
suguna-gunasekaran <sugunag.2006@gmail.com>
2026-08-04 10:51:06 +0530

    Refactor delivery drone policy iteration script

--- Lab-06.py
+++ Lab-06.py
@@ -1,3 +1,7 @@
+# An online platform uses bandit algorithms to decide which advertisements to show to users.
+# Implement epsilon-greedy, UCB, and Thompson Sampling algorithms. Use a Python script to
+# determine which algorithm results in the highest click-through rate over time.
+
 # Policy Iteration for Delivery Drone
 
 # Grid Size
