8cb2ee2a7612233a65a81317d4282610a6c1e71a
suguna-gunasekaran <sugunag.2006@gmail.com>
2026-08-04 10:48:01 +0530

    Update Lab-02.py

--- Lab-02.py
+++ Lab-02.py
@@ -1,3 +1,7 @@
+# A robot navigates a warehouse to pick and place items. Define states (locations in the
+# warehouse), actions (move in four directions), and rewards (picking an item: +2, reaching
+# the goal: +5, hitting an obstacle: -2). Implement a policy evaluation algorithm to determine
+# the value function for a given policy in Python.
 # Policy Evaluation for Warehouse Robot
 
 # Input Grid Size
